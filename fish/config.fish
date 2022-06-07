a2646c45cf0ae99a029bf8a2870bda944c4644bd
Kamal Chandra Upreti <kamalchettri1996@gmail.com>
2022-06-07 14:50:20 +0545

    Update  fish and i3 config

--- fish/config.fish
+++ fish/config.fish
@@ -1,20 +1,26 @@
-#aliases
+# custom aliases
 abbr -a cr 'cargo run'
 abbr -a nv 'nvim'
+abbr -a cl 'clear'
+
 abbr -a l 'exa'
 abbr -a ls 'exa'
 abbr -a ll 'exa -l'
 abbr -a lll 'exa -la'
-abbr -a lst 'exa -T'
+abbr -a lt 'exa -T'
+
 abbr -a pi 'paru -Sy'
 abbr -a pr 'paru -Rcns'
 abbr -a pu 'paru -Syu'
-abbr -a rb 'sudo reboot'
-abbr -a sd 'sudo shutdown now'
-abbr -a cl 'clear'
 
-# add cargo to PATH
+abbr -a gs 'git status'
+abbr -a ga 'git add'
+abbr -a gc 'git commit -m'
+abbr -a gp 'git push'
+
+# append to PATH
 set PATH $PATH ~/.cargo/bin
+set PATH $PATH ~/.local/bin
 
 function fish_greeting
 	# neofetch
--- fish/functions/fzf_key_bindings.fish
+++
@@ -1,1 +0,0 @@
-/home/m3kamal/.fzf/shell/key-bindings.fish
