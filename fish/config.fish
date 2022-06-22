11e1cda301116c47a474afe70286d9cebe554c44
Kamal Chandra Upreti <kamalchettri1996@gmail.com>
2022-06-22 18:43:26 +0545

    Remove aliases and tidy up

--- fish/config.fish
+++ fish/config.fish
@@ -1,33 +1,20 @@
-#==============================================
-# Fish Shell config
-#==============================================
-#  Written by Kamal C. Upreti
-#  (github.com/kml96)
-#==============================================
-# custom aliases
-abbr -a cr 'cargo run'
+# aliases
 abbr -a nv 'nvim'
-abbr -a cl 'clear'
-
 abbr -a l 'exa'
 abbr -a ls 'exa'
 abbr -a ll 'exa -l'
 abbr -a lll 'exa -la'
 abbr -a lt 'exa -T'
-
 abbr -a pi 'paru -Sy'
 abbr -a pr 'paru -Rcns'
 abbr -a pu 'paru -Syu'
 
-abbr -a gs 'git status'
-abbr -a ga 'git add'
-abbr -a gc 'git commit -m'
-abbr -a gp 'git push'
-
 # append to PATH
 set PATH $PATH ~/.cargo/bin
 set PATH $PATH ~/.local/bin
+set PATH $PATH ~/.npm-global/bin
 
+# initial greeting
 function fish_greeting
 	# neofetch
 end
