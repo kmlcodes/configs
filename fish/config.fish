e5cc2e8795758bcb781bbe5d890fef97a69bf969
kmlcodes <kamalchettri1996@gmail.com>
2022-07-09 00:12:27 +0000

    Update fish config & add config dir

--- fish/config.fish
+++ fish/config.fish
@@ -5,14 +5,12 @@ abbr -a ls 'exa'
 abbr -a ll 'exa -l'
 abbr -a lll 'exa -la'
 abbr -a lt 'exa -T'
-abbr -a pi 'paru -Sy'
-abbr -a pr 'paru -Rcns'
-abbr -a pu 'paru -Syu'
+abbr -a pi 'paru -S'
+abbr -a pr 'paru -R'
 
 # append to PATH
 set PATH $PATH ~/.cargo/bin
 set PATH $PATH ~/.local/bin
-set PATH $PATH ~/.npm-global/bin
 
 # initial greeting
 function fish_greeting
